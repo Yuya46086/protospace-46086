3586406701bab0ec39a45fba1884b4a3e917f5e7
Yuya46086 <fuji40726@gmail.com>
2025-08-18 20:31:45 +0900

    コメントフォームに関する修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,17 @@
 class CommentsController < ApplicationController
   def create
     @prototype = Prototype.find(params[:prototype_id])
-    @comment = Comment.new(comment_params.merge(user_id: current_user.id, prototype_id: @prototype.id))
+    @comment = Comment.new(comment_params.merge(user_id: current_user.id, prototype_id: params[:prototype_id]))
     if @comment.save
       redirect_to prototype_path(@comment.prototype)
     else
       render "prototypes/show", status: :unprocessable_entity
     end
   end
-end
 
-private
+  private
 
-def comment_params
-  params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+  end
 end
